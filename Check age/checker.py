a4fd7145755ad699d1eaf411beef19e87c0a47b6
Adithya-Dileep <Shugyo23@gmail.com>
2025-10-10 14:42:59 +0400

    Add files via upload

---
+++ Check age/checker.py
@@ -0,0 +1,7 @@
+age = int(input('Enter the age'))
+if age >=5 and age <=10:
+    print('you are allowed to enter the park ')
+elif age >10:
+    print('you are not allowed to enter the park')
+else:
+    print('invalid input')
